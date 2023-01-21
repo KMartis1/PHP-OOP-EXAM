3117866f7925335cd9e7e6f3def04a5e73aa804f
KMartis1 <martynas.keturakis@gmail.com>
2023-01-21 17:42:37 +0200

    fixed class, created newObj in index, on index expressed all needed values

--- index.php
+++ index.php
@@ -2,8 +2,18 @@
 
 require_once 'vendor/autoload.php';
 use \Marty\OopExam\module\Employee;
+use \Marty\OopExam\module\Intern;
+use \Marty\OopExam\module\Manager;
 
-$employee1 = new Employee("Martynas", 800);
-$employee1->setExperience(2);
-echo $employee1->toString($employee1) . PHP_EOL;
-echo $employee1->getExperience();
+$Intern1 = new Intern("Jonas", 500);
+$Manager1 = new Manager("Kazys", 1500,5);
+var_dump($Intern1) . PHP_EOL;
+echo ("------------------------") . PHP_EOL;
+var_dump($Manager1) . PHP_EOL;
+echo ("------------------------") . PHP_EOL;
+echo $Intern1->calculateSalary() . PHP_EOL;
+echo $Manager1->calculateSalary() . PHP_EOL;
+echo $Intern1->getTypeDescription() . PHP_EOL;
+echo $Manager1->getTypeDescription() . PHP_EOL;
+$Intern2 = clone $Intern1;
+echo $Intern2->getId();
--- src/module/Manager.php
+++ src/module/Manager.php
@@ -4,5 +4,25 @@ namespace Marty\OopExam\module;
 
 class Manager extends Employee
 {
+    public const EMPLOYEE_TYPE = 2;
+
+    public int $owerTimeHours;
+
+    public function __construct(string $name, int $salary, int $owerTimeHours)
+    {
+        $this->name = $name;
+        $this->salary= $salary;
+        $this->owerTimeHours = $owerTimeHours;
+        parent::__construct($name, $salary, $owerTimeHours);
+    }
+
+    public function calculateSalary()
+    {
+        return $this->salary + (($this->salary / 40 * $this->owerTimeHours) * 2);
+    }
+    public static function getTypeDescription(): string
+    {
+        return "Darbuotojo tipas - " . self::EMPLOYEE_TYPE;
+    }
 
 }
